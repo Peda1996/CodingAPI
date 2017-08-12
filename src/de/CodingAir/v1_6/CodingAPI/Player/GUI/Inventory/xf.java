fe74bd430d5b125f4c95afb47444bcfb1744c95c
BubbleEgg <BubbleEgg@freenet.de>
2017-08-12 16:32:20 +0200

    test

---
+++ src/de/CodingAir/v1_6/CodingAPI/Player/GUI/Inventory/xf.java
@@ -0,0 +1,11 @@
+package de.CodingAir.v1_6.CodingAPI.Player.GUI.Inventory;
+
+/**
+ * Removing of this disclaimer is forbidden.
+ *
+ * @author BubbleEgg
+ * @verions: 1.0.0
+ **/
+
+public class xf {
+}
